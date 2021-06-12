1e7a412c2226519c5c14d6bd729db28e2943e704
Gonzalo Cubillas <gonzalo.cubillas15@gmail.com>
2021-06-12 17:36:26 -0300

    se agrego la logica en JS

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
 
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
     fetchData()
 } )
+
+items.addEventListener('click', e =>{
+    addCarrito(e)
+})
 
 const fetchData = async ()=>{
     try{
@@ -18,5 +23,23 @@ const fetchData = async ()=>{
 }
 
 const pintarCArd = data =>{
-        console.log(data)
+    data.forEach(producto =>{
+      templateCard.querySelector('h5').textContent = producto.title
+      templateCard.querySelector('p').textContent = producto.precio
+      templateCard.querySelector('img').setAttribute('src', producto.imagen)
+      templateCard.querySelector('.btn-primary').dataset.id = producto.id
+
+      const clone = templateCard.cloneNode(true)
+      fragment.appendChild(clone)
+    })    
+    items.appendChild(fragment)
+   
+}
+
+const addCarrito = e =>{
+    //console.log(e.target)
+    console.log(e.target.classList.contains('btn-primary'))
+    //if(e.target.classList.contains('btn-primary')){
+
+    }
 }
